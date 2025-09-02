2f322e7cbd97ad29ed0fef297654ec06fd734f2e
_den <_den@outlook.com>
2025-09-02 14:13:37 +1000

    save/delete image info to db

--- app/main.py
+++ app/main.py
@@ -24,8 +24,9 @@ async def root(request: Request):
 @app.post("/")
 async def upload(request: Request, file: UploadFile = File(...)):
     content = await file.read(MAX_FILE_SIZE + 1)
+    file_path = Path(file.filename)
 
-    if not is_allowed_file(Path(file.filename)):
+    if not is_allowed_file(file_path):
         return templates.TemplateResponse(
             "index.html",
             {
@@ -45,13 +46,24 @@ async def upload(request: Request, file: UploadFile = File(...)):
                 "detail": "Content Too Large",
             }
         )    
-    new_filename = get_unique_name(content, Path(file.filename))
+    new_filename = get_unique_name(content, file_path)
     file_url = f"{request.base_url}image/{new_filename}"
 
     images_dir = Path("images")
     images_dir.mkdir(exist_ok=True)
     path = images_dir/new_filename
     path.write_bytes(content)
+
+    conn = get_conn()
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO images(
+	            filename, original_name, size, file_type)
+	        VALUES (%s, %s, %s, %s);
+            """, (path.stem, file_path.stem, path.stat().st_size, path.suffix)
+        )
+        conn.commit()
+
     return templates.TemplateResponse(
         "index.html",
         {
@@ -96,6 +108,14 @@ async def delete(request: Request, api_key: str = Form(...), image_name: str = F
             path = images_dir/image_name
             if path.exists():
                 path.unlink()
+                conn = get_conn()
+            with conn.cursor() as cur:
+                cur.execute("""
+                    DELETE FROM images
+                    WHERE filename = %s;
+                    """, (path.stem,)
+                )
+                conn.commit()
             return await images(request, True)
 
 if __name__ == '__main__':
